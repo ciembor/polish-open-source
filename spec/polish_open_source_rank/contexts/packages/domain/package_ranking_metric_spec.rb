c151da28357b0b440dd1f653fded9281c7eb8b7a
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2026-05-24 22:53:59 +0200

    Add Packagist download rankings

--- spec/polish_open_source_rank/contexts/packages/domain/package_ranking_metric_spec.rb
+++ spec/polish_open_source_rank/contexts/packages/domain/package_ranking_metric_spec.rb
@@ -7,9 +7,17 @@ RSpec.describe PolishOpenSourceRank::Contexts::Packages::Domain::PackageRankingM
     expect(described_class.key_for_slug('top')).to eq('downloads_30d')
     expect(described_class.supported_key?(:downloads_total)).to be(true)
     expect(described_class.supported_key?(:unknown)).to be(false)
-    expect(described_class.slugs(ecosystem: 'npm')).to eq(%w[top])
-    expect(described_class.keys(ecosystem: 'crates')).to eq(%w[downloads_30d downloads_total])
-    expect(described_class.keys(ecosystem: 'pypi')).to eq([])
+    expect(
+      npm_slugs: described_class.slugs(ecosystem: 'npm'),
+      crates_keys: described_class.keys(ecosystem: 'crates'),
+      packagist_keys: described_class.keys(ecosystem: 'packagist'),
+      pypi_keys: described_class.keys(ecosystem: 'pypi')
+    ).to eq(
+      npm_slugs: %w[top],
+      crates_keys: %w[downloads_30d downloads_total],
+      packagist_keys: %w[downloads_30d downloads_total],
+      pypi_keys: []
+    )
     expect(described_class.supported_for_ecosystem?('npm', 'downloads_total')).to be(false)
     expect(described_class.slugs_pattern).to eq('top|downloads|dependents')
   end
