1cb993babaabdd19324f88a9777891a3173396ed
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2026-05-27 23:08:04 +0200

    Language icon placeholder.

---
+++ spec/polish_open_source_rank/web/presentation/language_path_helpers_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe PolishOpenSourceRank::Web::Presentation::LanguagePathHelpers do
+  subject(:helper) do
+    Class.new do
+      include PolishOpenSourceRank::Web::Presentation::LanguagePathHelpers
+    end.new
+  end
+
+  describe '#language_initial' do
+    it 'returns the uppercased first character' do
+      expect(helper.language_initial('bitbake')).to eq('B')
+    end
+  end
+end
